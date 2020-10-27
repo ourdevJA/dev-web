9f853f03cb94074320c51500bee202babd9cebdf
maxxii <dorian-94@mail.ru>
2020-10-27 12:51:25 +0300

    Start project

---
+++ src/test/java/ourdevproject/firststartup/FirststartupApplicationTests.java
@@ -0,0 +1,13 @@
+package ourdevproject.firststartup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirststartupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
